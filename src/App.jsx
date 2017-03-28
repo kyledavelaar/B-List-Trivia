75a3fa80106d7053e91d9df453446ea30dda210d
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-03-28 12:02:52 -0700

    add audio and put images in separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import laurence from './images/laurence-fishbourne.png';
-import aaron from './images/aaron-eckhardt.png';
+import ReactAudioPlayer from 'react-audio-player';
+import Answers from './answers';
+// import laurence from './images/laurence-fishbourne.png';
+// import aaron from './images/aaron-eckhardt.png';
+// import end from './images/end.jpg';
+import correct from './sound/correct.mp3';
+import wrong from './sound/wrong.mp3';
+import gthang from './sound/gthang.mp3';
 
-let answer = [
-  {
-    answer1: 'mario lopez', 
-    answer2: 'billy boy',
-    answer3: 'jeffy',
-    answer4: 'stacy',
-    correct: 'mario lopez',
-    image: laurence,
-  },
-  {
-    answer1: 'zeus', 
-    answer2: 'aeuro',
-    answer3: 'triton',
-    answer4: 'magdalena',
-    correct: 'zeus',
-    image: aaron,
-  },
-  {
-    answer1: 'mario lopez', 
-    answer2: 'billy boy',
-    answer3: 'jeffy',
-    answer4: 'stacy',
-    correct: 'mario lopez',
-    image: laurence,
-  },
-  {
-    answer1: 'zeus', 
-    answer2: 'aeuro',
-    answer3: 'triton',
-    answer4: 'magdalena',
-    correct: 'zeus',
-    image: aaron,
-  },
-]
 
 
 export default class App extends Component {
@@ -49,41 +21,61 @@ export default class App extends Component {
   }
 
   checkAnswer(clicked) {
-    console.log( clicked );
-    if (clicked === answer[this.state.round].correct) {
+    console.log(Answers.answer[4].answer1)
+    if (clicked === Answers.answer[this.state.round].correct) {
       this.updateScoreAndChangeRound();
+      // return (
+      //   <audio src={correct} autoplay></audio>
+      // )
+      
     } else {
       this.moveToNextRound();
+      //return wrong.play();
+    }
+    if (this.state.round === 5) {
+      //end game and declare winner/loser 
+      //keeps going even if get wrong answer at last one
+         //this.setState(Object.assign({}, this.state, {score: this.state.score + 1}))
+      console.log('game over')
     }
   }
 
   updateScoreAndChangeRound() {
-    //this.setState( {score: this.state.score + 1, round: this.state.round + 1})
+    
     this.setState(Object.assign({}, this.state, {score: this.state.score + 1, round: this.state.round + 1}))
+    return (
+        <audio src={correct} autoplay></audio>
+      )
   }
 
   moveToNextRound() {
     this.setState(Object.assign({}, this.state, {round: this.state.round + 1}))
   }
 
+  componentDidMount() {
+    
+  }
 
   render() {
     return (
       <div className="App">
+     {/* <ReactAudioPlayer src={gthang} autoPlay />*/}
+             <audio src={correct} autoPlay></audio>
+
         <div className="image-holder">
-          <img className="img" src={answer[this.state.round].image} alt=""/>
+          <img className="img" src={Answers.answer[this.state.round].image} alt=""/>
           <div className="buttons-holder">
-            <button className="btn" onClick={ this.checkAnswer.bind(this, answer[this.state.round].answer1)}>
-              {answer[this.state.round].answer1}
+            <button className="btn" onClick={ this.checkAnswer.bind(this, Answers.answer[this.state.round].answer1)}>
+              {Answers.answer[this.state.round].answer1}
             </button>
-            <button className="btn" onClick={ this.checkAnswer.bind(this, answer[this.state.round].answer2)}>
-              {answer[this.state.round].answer2}
+            <button className="btn" onClick={ this.checkAnswer.bind(this, Answers.answer[this.state.round].answer2)}>
+              {Answers.answer[this.state.round].answer2}
             </button>
-            <button className="btn" onClick={ this.checkAnswer.bind(this, answer[this.state.round].answer3)}>
-              {answer[this.state.round].answer3}
+            <button className="btn" onClick={ this.checkAnswer.bind(this, Answers.answer[this.state.round].answer3)}>
+              {Answers.answer[this.state.round].answer3}
             </button>
-            <button className="btn" onClick={ this.checkAnswer.bind(this, answer[this.state.round].answer4)}>
-              {answer[this.state.round].answer4}
+            <button className="btn" onClick={ this.checkAnswer.bind(this, Answers.answer[this.state.round].answer4)}>
+              {Answers.answer[this.state.round].answer4}
             </button>
  
             <div className="score-holder">
---
+++ src/answers.js
@@ -0,0 +1,60 @@
+import laurence from './images/laurence-fishbourne.png';
+import aaron from './images/aaron-eckhardt.png';
+import end from './images/end.jpg';
+
+
+module.exports = {
+
+  answer : [
+    {
+      answer1: 'mario lopez', 
+      answer2: 'billy boy',
+      answer3: 'jeffy',
+      answer4: 'stacy',
+      correct: 'mario lopez',
+      image: laurence,
+    },
+    {
+      answer1: 'zeus', 
+      answer2: 'aeuro',
+      answer3: 'triton',
+      answer4: 'magdalena',
+      correct: 'zeus',
+      image: aaron,
+    },
+    {
+      answer1: 'mario lopez', 
+      answer2: 'billy boy',
+      answer3: 'jeffy',
+      answer4: 'stacy',
+      correct: 'mario lopez',
+      image: laurence,
+    },
+    {
+      answer1: 'zeus', 
+      answer2: 'aeuro',
+      answer3: 'triton',
+      answer4: 'magdalena',
+      correct: 'zeus',
+      image: aaron,
+    },
+    {
+      answer1: 'piggy', 
+      answer2: 'marco',
+      answer3: 'zarra',
+      answer4: 'maggie',
+      correct: 'piggy',
+      image: laurence,
+    },
+    {
+    answer1: 'piggy', 
+    answer2: 'marco',
+    answer3: 'zarra',
+    answer4: 'maggie',
+    correct: 'piggy',
+    winMessage: 'You Win', 
+    loseMessage: 'You Lose', 
+    image: end,
+  },
+  ]
+}
